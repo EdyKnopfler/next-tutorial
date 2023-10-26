bd515aaa3ef1057e52aa3cfe6d93a88863f3179b
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-26 15:54:38 -0300

    Imagem com inúmeros beneffícios de otimização

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,13 @@ import { lusitana } from './ui/fonts';
 
 // Módulo CSS
 import styles from './ui/home.module.css'
+import Image from 'next/image';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -43,7 +44,22 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          {/* 
+            * Preventing layout shift automatically when images are loading.
+            * Resizing images to avoid shipping large images to devices with a smaller viewport.
+              * It generates multiple sizes of each image for different devices. 
+            * Lazy loading images by default (images load as they enter the viewport).
+            * Serving images in modern formats, like WebP and AVIF, when the browser supports it.
+            
+            Tudo isso inteiramente de grátis e sem nenhum custo adicional!
+          */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          /> {/* imagem vindo da /public */}
         </div>
       </div>
     </main>
