948d705705d530d11950567c12b86471babec618
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-26 16:52:48 -0300

    Destacando página atual no menu com clsx e o hook usePathname

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,3 +1,6 @@
+// O uso do hook (usePathname) força que o componente seja renderizado no cliente
+'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
@@ -5,6 +8,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -19,6 +23,8 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
@@ -30,6 +36,9 @@ export default function NavLinks() {
             href={link.href}
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href
+              }
             )}
           >
             <LinkIcon className="w-6" />
