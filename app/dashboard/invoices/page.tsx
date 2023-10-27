a24862874900b7830dc7fa66b4d116b2b6c0e72c
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-27 17:46:54 -0300

    Visual

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -27,6 +27,8 @@ export default async function Page({ searchParams }: { searchParams?: SearchPara
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Suspense key={query + currentPage} fallback={<InvoiceSkeleton />}>
           <InvoicesTable query={query} currentPage={currentPage} />
         </Suspense>
