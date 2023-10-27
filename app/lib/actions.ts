14816c0d030c5e8ffa6363ce623ae1955282cf3e
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-27 18:07:09 -0300

    Validando formulários xcom zod

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,20 @@
 // <form action={function} children=...>
 'use server';
 
+import { z } from 'zod';
+
+import db from './db';
+
+const InvoiceSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
   const rawFormData = {
     customerId: formData.get('customerId'),
@@ -8,7 +22,17 @@ export async function createInvoice(formData: FormData) {
     status: formData.get('status'),
   };
 
-  console.log(rawFormData)
+  const { customerId, amount, status } = CreateInvoice.parse(rawFormData);
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await db.query(
+    `
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES ($1, $2, $3, $4)
+    `,
+    [customerId, amountInCents, status, date]
+  );
 }
 
 export async function deleteInvoice() {
