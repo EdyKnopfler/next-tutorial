2424496b6bfbd386fa89ea018e4f4ba37912b7ed
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-27 17:58:13 -0300

    Forms e server actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,4 +1,15 @@
 // <form action={function} children=...>
+'use server';
+
+export async function createInvoice(formData: FormData) {
+  const rawFormData = {
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  };
+
+  console.log(rawFormData)
+}
+
 export async function deleteInvoice() {
-  'use server';
 }
