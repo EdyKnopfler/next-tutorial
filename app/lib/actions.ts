f5eae4820d480ea9f0d045cd22a3d269691ec47e
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-27 21:37:09 -0300

    Edição básica

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -16,6 +16,7 @@ const InvoiceSchema = z.object({
 });
 
 const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
+const UpdateInvoice = InvoiceSchema.omit({ date: true });
 
 export async function createInvoice(formData: FormData) {
   const rawFormData = {
@@ -34,6 +35,30 @@ export async function createInvoice(formData: FormData) {
       VALUES ($1, $2, $3, $4)
     `,
     [customerId, amountInCents, status, date]
+  );
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(formData: FormData) {
+  // Aqui nada de autenticação ou verificação de token CSRF por enquanto
+  const { id, customerId, amount, status } = UpdateInvoice.parse({
+    id: formData.get('id'),
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await db.query(
+    `
+      UPDATE invoices
+      SET customer_id = $1, amount = $2, status = $3
+      WHERE id = $4
+    `,
+    [customerId, amountInCents, status, id]
   );
 
   revalidatePath('/dashboard/invoices');
